f1c8618ed4db6d8a2ae3747719bdd431f42bb0f4
Chris Jenkins <chris.jenkins@trustpower.co.nz>
2021-07-07 22:09:35 +1200

    Revert "Removed target from gitignore"

--- target/compiled/my_new_project/models/analytics/customer_address.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    customer.first_name,
-    customer.last_name,
-    customer.customer_id,
-    address.address,
-    address.address2
-FROM "dvdrental"."public"."customer" AS customer
-LEFT JOIN "dvdrental"."public"."address" AS address
-    ON address.address_id = customer.address_id
--- target/compiled/my_new_project/models/analytics/customer_film_categories.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    film_category_name.description,
-    customer_categories.customer_name
-FROM "dvdrental"."dbt"."customer_categories"
-LEFT JOIN "dvdrental"."dbt"."film_category_name"
-    ON film_category_name.category_name = customer_categories.category_name
